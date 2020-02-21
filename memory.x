d6011b65b191a44a158f7af857ddc004abef46a2
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-02-21 11:12:59 -0800

    WIP: adding SPI support

--- memory.x
+++ memory.x
@@ -1,29 +1,19 @@
-/* Memory layout for stm32h743 */
+/* Memory layout for stm32f401 */
 MEMORY
 {
+  /* NOTE K = 1024 bytes */
   /* FLASH and RAM are mandatory memory regions */
+
   FLASH  : ORIGIN = 0x08000000, LENGTH = 256K
-  FLASH1 : ORIGIN = 0x08100000, LENGTH = 1024K
-  RAM    : ORIGIN = 0x20000000, LENGTH = 128K
+  RAM    : ORIGIN = 0x20000000, LENGTH = 64K
 
-  /* AXISRAM */
-  AXISRAM : ORIGIN = 0x24000000, LENGTH = 512K
-
-  /* SRAM */
-  SRAM1 : ORIGIN = 0x30000000, LENGTH = 128K
-  SRAM2 : ORIGIN = 0x30020000, LENGTH = 128K
-  SRAM3 : ORIGIN = 0x30040000, LENGTH = 32K
-  SRAM4 : ORIGIN = 0x38000000, LENGTH = 64K
-
-  /* Backup SRAM */
-  BSRAM : ORIGIN = 0x38800000, LENGTH = 4K
-
-  /* Instruction TCM */
-  ITCM  : ORIGIN = 0x00000000, LENGTH = 64K
 }
 
-/* The location of the stack can be overridden using the
-   `_stack_start` symbol.  Place the stack at the end of RAM */
+/*
+This is where the call stack will be allocated.
+The stack is of the full descending type.
+Place the stack at the end of RAM.
+*/
 _stack_start = ORIGIN(RAM) + LENGTH(RAM);
 
 /* The location of the .text section can be overridden using the
--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,17 @@ use core::cell::RefCell;
 use cortex_m::interrupt::{self, Mutex};
 
 #[cfg(feature = "stm32h7x")]
-use stm32h7xx_hal as processor_hal;
+use stm32h7xx_hal as p_hal;
 
 #[cfg(feature = "stm32f4x")]
-use stm32f4xx_hal as processor_hal;
+use stm32f4xx_hal as p_hal;
 
 #[cfg(feature = "stm32f3x")]
-use stm32f3xx_hal as processor_hal;
+use stm32f3xx_hal as p_hal;
 
-use processor_hal::prelude::*;
-use processor_hal::stm32 as pac;
+use p_hal::prelude::*;
+use p_hal::stm32 as pac;
+use p_hal::stm32 as stm32;
 use pac::I2C1;
 //use pac::DWT;
 
@@ -31,11 +32,7 @@ extern crate cortex_m_rt;
 
 use cortex_m_rt::{entry, ExceptionFrame};
 
-use processor_hal::hal::digital::v2::OutputPin;
-use processor_hal::hal::digital::v2::ToggleableOutputPin;
-
-
-use cmsis_rtos2;//::{osThreadAttr_t, osPriority_t_osPriorityNormal};
+use cmsis_rtos2;
 
 
 #[cfg(debug_assertions)]
@@ -56,13 +53,17 @@ use cortex_m_log::{d_println};
 //use cortex_m_semihosting;
 
 
-use processor_hal::gpio::GpioExt;
-use processor_hal::rcc::RccExt;
+use p_hal::gpio::GpioExt;
+use p_hal::rcc::RccExt;
 
 use core::ops::{DerefMut};
 
 use core::ptr::{null, null_mut};
-use bno080::*;
+use bno080::wrapper::BNO080;
+// use bno080::interface::{I2cInterface};
+use bno080::interface::{SpiInterface};
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::ToggleableOutputPin;
 
 
 const SYSCLOCK_TICK_RATE_HZ: u16 = 1000;
@@ -80,8 +81,8 @@ pub static SystemCoreClock: u32 =  8_000_000; //same as stm32f3xx_hal::rcc::HSI
 #[cfg(feature = "stm32f4x")]
 #[allow(non_upper_case_globals)]
 #[no_mangle]
-pub static SystemCoreClock: u32 =  16_000_000; //same as stm32f4xx_hal::rcc::HSI
-// pub static SystemCoreClock: u32 =  25_000_000; // eg stm32f401 board with 25 MHz xtal HSE
+// pub static SystemCoreClock: u32 =  16_000_000; //same as stm32f4xx_hal::rcc::HSI
+pub static SystemCoreClock: u32 =  25_000_000; // eg stm32f401 board with 25 MHz xtal HSE
 // pub static SystemCoreClock: u32 =  8_000_000; // eg stm32f407 board with 8 MHz xtal HSE
 
 #[cfg(feature = "stm32h7x")]
@@ -92,25 +93,41 @@ pub static SystemCoreClock: u32 = 48_000_000; //stm32h743 HSI (48 MHz)
 
 
 #[cfg(feature = "stm32f3x")]
-type ImuI2cPortType = processor_hal::i2c::I2c<I2C1,
-  (processor_hal::gpio::gpiob::PB8<processor_hal::gpio::AF4>,
-   processor_hal::gpio::gpiob::PB9<processor_hal::gpio::AF4>)
+type ImuI2cPortType = p_hal::i2c::I2c<I2C1,
+  (p_hal::gpio::gpiob::PB8<p_hal::gpio::AF4>,
+   p_hal::gpio::gpiob::PB9<p_hal::gpio::AF4>)
 >;
 
 #[cfg(feature = "stm32f4x")]
-type ImuI2cPortType = processor_hal::i2c::I2c<I2C1,
- (processor_hal::gpio::gpiob::PB8<processor_hal::gpio::AlternateOD<processor_hal::gpio::AF4>>,
-   processor_hal::gpio::gpiob::PB9<processor_hal::gpio::AlternateOD<processor_hal::gpio::AF4>>)
+pub type ImuI2cPortType = p_hal::i2c::I2c<I2C1,
+ (p_hal::gpio::gpiob::PB8<p_hal::gpio::AlternateOD<p_hal::gpio::AF4>>,
+  p_hal::gpio::gpiob::PB9<p_hal::gpio::AlternateOD<p_hal::gpio::AF4>>)
 >;
 
 #[cfg(feature = "stm32h7x")]
-type ImuI2cPortType = processor_hal::i2c::I2c<I2C1,
-  (processor_hal::gpio::gpiob::PB8<processor_hal::gpio::Alternate<processor_hal::gpio::AF4>>,
-   processor_hal::gpio::gpiob::PB9<processor_hal::gpio::Alternate<processor_hal::gpio::AF4>>)
+pub type ImuI2cPortType = p_hal::i2c::I2c<I2C1,
+  (p_hal::gpio::gpiob::PB8<p_hal::gpio::Alternate<p_hal::gpio::AF4>>,
+   p_hal::gpio::gpiob::PB9<p_hal::gpio::Alternate<p_hal::gpio::AF4>>)
+>;
+// type ImuDriverType = bno080::wrapper::BNO080<I2cInterface<ImuI2cPortType>>;
+
+#[cfg(feature = "stm32f4x")]
+pub type SpiPortType = p_hal::spi::Spi<stm32::SPI1,
+  (
+    p_hal::gpio::gpiob::PB3<p_hal::gpio::Alternate<p_hal::gpio::AF5>>,
+    p_hal::gpio::gpiob::PB4<p_hal::gpio::Alternate<p_hal::gpio::AF5>>,
+    p_hal::gpio::gpiob::PB5<p_hal::gpio::Alternate<p_hal::gpio::AF5>>,
+  )
+  // (
+  // PB3<Alternate<PushPull>>,
+  //   PB4<Input<Floating>>,
+  //   PB5<Alternate<PushPull>>,
+  // )
 >;
 
+type ChipSelectPinType = p_hal::gpio::gpiob::PB2<p_hal::gpio::Output<p_hal::gpio::PushPull>>;
 
-type ImuDriverType = bno080::BNO080<ImuI2cPortType>;
+type ImuDriverType = bno080::wrapper::BNO080<SpiInterface<SpiPortType, ChipSelectPinType>>;
 
 
 
@@ -120,18 +137,18 @@ type DebugLog = cortex_m_log::printer::dummy::Dummy;
 
 
 #[cfg(feature = "stm32f3x")]
-type GpioTypeUserLed1 =  processor_hal::gpio::gpiob::PB6<processor_hal::gpio::Output<processor_hal::gpio::PushPull>>;
+type GpioTypeUserLed1 =  p_hal::gpio::gpiob::PB6<p_hal::gpio::Output<p_hal::gpio::PushPull>>;
 
 #[cfg(feature = "stm32f4x")]
-type GpioTypeUserLed1 =  processor_hal::gpio::gpioc::PC13<processor_hal::gpio::Output<processor_hal::gpio::PushPull>>; //stm32f401CxUx
-// type GpioTypeUserLed1 =  processor_hal::gpio::gpiod::PD12<processor_hal::gpio::Output<processor_hal::gpio::PushPull>>; //stm32f407disco
+type GpioTypeUserLed1 =  p_hal::gpio::gpioc::PC13<p_hal::gpio::Output<p_hal::gpio::PushPull>>; //stm32f401CxUx
+// type GpioTypeUserLed1 =  p_hal::gpio::gpiod::PD12<p_hal::gpio::Output<p_hal::gpio::PushPull>>; //stm32f407disco
 
 #[cfg(feature = "stm32h7x")]
-type GpioTypeUserLed1 =  processor_hal::gpio::gpiob::PB0<processor_hal::gpio::Output<processor_hal::gpio::PushPull>>;
+type GpioTypeUserLed1 =  p_hal::gpio::gpiob::PB0<p_hal::gpio::Output<p_hal::gpio::PushPull>>;
 
 
 static USER_LED_1:  Mutex<RefCell<Option< GpioTypeUserLed1>>> = Mutex::new(RefCell::new(None));
-static APP_DELAY_SOURCE: Mutex<RefCell<Option<  processor_hal::delay::Delay >>> = Mutex::new(RefCell::new(None));
+static APP_DELAY_SOURCE: Mutex<RefCell<Option<  p_hal::delay::Delay >>> = Mutex::new(RefCell::new(None));
 static IMU_DRIVER: Mutex<RefCell<Option< ImuDriverType>>> = Mutex::new(RefCell::new(None));
 
 //static APP_ITM: Mutex<RefCell<Option< cortex_m::peripheral::ITM >>> = Mutex::new(RefCell::new(None));
@@ -148,9 +165,8 @@ fn DefaultHandler(_irqn: i16) {
 
 // cortex-m-rt calls this for serious faults.  can set a breakpoint to debug
 #[exception]
-fn HardFault(_ef: &ExceptionFrame) -> ! {
-  d_println!(get_debug_log(),  "HardFault: {:?}", _ef);
-  loop { }
+fn HardFault(ef: &ExceptionFrame) -> ! {
+  panic!("HardFault: {:?}", ef);
 }
 
 #[exception]
@@ -259,16 +275,17 @@ pub fn spin_imu_driver() -> ! {
   }
 
   loop {
-    let mut msg_count = 0;
+    // let mut msg_count = 0;
     interrupt::free(|cs| {
       if let Some(ref mut imu_driver) = IMU_DRIVER.borrow(cs).borrow_mut().deref_mut() {
-        msg_count = imu_driver.get_received_packet_count();
+        //msg_count = imu_driver.get_received_packet_count();
         //msg_count = imu_driver.handle_all_messages();
+        imu_driver.handle_one_message();
       }
     });
-    if msg_count < 1 {
-      cmsis_rtos2::rtos_os_delay(tick_interval);
-    }
+
+    cmsis_rtos2::rtos_os_delay(tick_interval);
+
   }
 
 }
@@ -307,7 +324,7 @@ fn setup_peripherals_f3x()  {
   //use the existing sysclk
   let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-  let delay_source =  processor_hal::delay::Delay::new(cp.SYST, clocks);
+  let delay_source =  p_hal::delay::Delay::new(cp.SYST, clocks);
 
   let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
   //let mut gpiod = dp.GPIOD.split(&mut rcc.ahb);
@@ -327,9 +344,9 @@ fn setup_peripherals_f3x()  {
       .into_af4(&mut gpiob.moder, &mut gpiob.afrh);
   //sda.internal_pull_up(&mut gpiob.pupdr, true);
 
-  let imu_i2c_port = processor_hal::i2c::I2c::i2c1(
+  let imu_i2c_port = p_hal::i2c::I2c::i2c1(
     dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
-  let imu_driver = BNO080::new(imu_i2c_port);
+  let imu_driver = BNO080::new_with_interface(imu_i2c_port);
 
   //store shared peripherals
   interrupt::free(|cs| {
@@ -348,11 +365,13 @@ fn setup_peripherals_f4x()  {
   // Set up the system clock
   let rcc = dp.RCC.constrain();
   // HSI: use default internal oscillator
-   let clocks = rcc.cfgr.freeze();
+  //  let clocks = rcc.cfgr.freeze();
   // HSE: external crystal oscillator must be connected
-//  let clocks = rcc.cfgr.use_hse(SystemCoreClock.hz()).freeze();
+  let clocks = rcc.cfgr.use_hse(SystemCoreClock.hz()).freeze();
+  // let clocks = rcc.cfgr.sysclk(SystemCoreClock.hz()).freeze();
+  // let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
 
-  let delay_source =  processor_hal::delay::Delay::new(cp.SYST, clocks);
+  let delay_source =  p_hal::delay::Delay::new(cp.SYST, clocks);
 
   let gpiob = dp.GPIOB.split();
   let gpioc = dp.GPIOC.split();
@@ -363,20 +382,50 @@ fn setup_peripherals_f4x()  {
   //set initial states of user LEDs
   user_led1.set_high().unwrap();
 
+  //SPI1_SCK
+  let sck = gpiob.pb3
+      .into_push_pull_output()
+      .into_alternate_af5();
+  //SPI1_MISO
+  let miso = gpiob.pb4
+      .into_floating_input()
+      .into_alternate_af5();
+  //SPI1_MOSI
+  let mosi = gpiob.pb5
+      .into_push_pull_output()
+      .into_alternate_af5();
 
-  // setup i2c1 and imu driver
-  // NOTE: eg f407 discovery board already has external pull-ups
- let scl = gpiob.pb8
-     .into_alternate_af4()
-     .internal_pull_up(true)
-     .set_open_drain();
+  let spi_port = p_hal::spi::Spi::spi1(
+    dp.SPI1, (sck, miso, mosi),
+    embedded_hal::spi::MODE_0, 3_000_000.hz(), clocks );
 
-  let sda = gpiob.pb9
-      .into_alternate_af4()
-      .internal_pull_up(true)
-      .set_open_drain();
-  let imu_i2c_port = processor_hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks);
-  let imu_driver = BNO080::new(imu_i2c_port);
+  // SPI chip select CS
+  let cs = gpiob.pb2
+      .into_push_pull_output();
+
+  let spi_iface = bno080::interface::SpiInterface::new(spi_port, cs);
+  let imu_driver = BNO080::new_with_interface(spi_iface);
+
+  // expected type
+  // `bno080::wrapper::BNO080<bno080::interface::spi::SpiInterface<stm32f4xx_hal::spi::Spi<_, (stm32f4xx_hal::gpio::gpiob::PB3<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::PushPull>>, stm32f4xx_hal::gpio::gpiob::PB4<stm32f4xx_hal::gpio::Input<stm32f4xx_hal::gpio::Floating>>, stm32f4xx_hal::gpio::gpiob::PB5<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::PushPull>>)>, stm32f4xx_hal::gpio::gpiob::PB2<stm32f4xx_hal::gpio::PushPull>>>`
+  // found type
+  // `bno080::wrapper::BNO080<bno080::interface::spi::SpiInterface<stm32f4xx_hal::spi::Spi<_, (stm32f4xx_hal::gpio::gpiob::PB3<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::AF5>>, stm32f4xx_hal::gpio::gpiob::PB4<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::AF5>>, stm32f4xx_hal::gpio::gpiob::PB5<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::AF5>>)>, stm32f4xx_hal::gpio::gpiob::PB2<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>>>>`
+  //
+
+  //  // setup i2c1 and imu driver
+ //  // NOTE: eg f407 discovery board already has external pull-ups
+ // let scl = gpiob.pb8
+ //     .into_alternate_af4()
+ //     .internal_pull_up(true)
+ //     .set_open_drain();
+ //
+ //  let sda = gpiob.pb9
+ //      .into_alternate_af4()
+ //      .internal_pull_up(true)
+ //      .set_open_drain();
+ //  let imu_i2c_port = p_hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks);//TODO raise to 4oo khz
+ //  let iface = I2cInterface::new(imu_i2c_port, bno080::interface::i2c::DEFAULT_ADDRESS);
+ //  let imu_driver = BNO080::new_with_interface(iface);
 
   //store shared peripherals
   interrupt::free(|cs| {
@@ -402,7 +451,7 @@ fn setup_peripherals_h7x()  {
   //use the existing sysclk
   let mut ccdr = rcc.freeze(vos, &dp.SYSCFG);
   let clocks = ccdr.clocks;
-  let delay_source =  processor_hal::delay::Delay::new(cp.SYST, clocks);
+  let delay_source =  p_hal::delay::Delay::new(cp.SYST, clocks);
 
   let gpiob = dp.GPIOB.split(&mut ccdr.ahb4);
 
@@ -411,11 +460,15 @@ fn setup_peripherals_h7x()  {
   user_led1.set_high().unwrap();
 
   // setup i2c1 and imu driver
-  let scl = gpiob.pb8.into_alternate_af4().internal_pull_up(true).set_open_drain();
-  let sda = gpiob.pb9.into_alternate_af4().internal_pull_up(true).set_open_drain();
+  let scl = gpiob.pb8.into_alternate_af4()
+      //.internal_pull_up(true)
+      .set_open_drain();
+  let sda = gpiob.pb9.into_alternate_af4()
+      //.internal_pull_up(true)
+      .set_open_drain();
   //  let imu_i2c_port = dp.I2C1.i2c((scl, sda), 400.khz(), clocks);
-  let imu_i2c_port = processor_hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 100.khz(), &ccdr);//TODO raise to 400
-  let imu_driver = BNO080::new(imu_i2c_port);
+  let imu_i2c_port = p_hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 100.khz(), &ccdr);//TODO raise to 400
+  let imu_driver = BNO080::new_with_interface(imu_i2c_port);
 
   //store shared peripherals
   interrupt::free(|cs| {
